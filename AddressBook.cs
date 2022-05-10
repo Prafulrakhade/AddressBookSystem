442f401763f77ecda171e04a82970c1a8fa0eec9
Praful <prafulrakhade02@gmail.com>
2022-05-10 12:12:39 +0530

    [praful] Added : Uc_2_Address Book Program File

--- AddressBook.cs
+++ AddressBook.cs
@@ -14,8 +14,21 @@ namespace AddressBookSystem
         public void AddPerson(Person p)
         {
             per.Add(p);
-            string jsonData = JsonConvert.SerializeObject(per);
+            string jsonData = JsonConvert.SerializeObject(per);  // it convert custom to Json.... Json Convert the Serialize Object data into the string data (i.e. covert from list to string ) 
             File.WriteAllText(@"C:\Users\HP\Bridgelabz-145\Assignments_\Day 9\AddressBookSystem\AddressBookSystem\AddressBokkResult.json", jsonData);
+            //File.WriteAllText string data stored in Json File.  
+        }
+        public AddressBook()
+        {
+            string json = File.ReadAllText(@"C:\Users\HP\Bridgelabz-145\Assignments_\Day 9\AddressBookSystem\AddressBookSystem\AddressBokkResult.json");
+            if (json.Length > 0)
+            {
+                per = JsonConvert.DeserializeObject<List<Person>>(json); //it convert Json to the custom (i.e. string to list)
+            }
+            else
+            {
+                per = new List<Person>();
+            }
         }
         public void Display()
         {
